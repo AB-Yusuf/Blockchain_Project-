a6776ea90d6f42e8e5e39f3ce5d43acc212e7ba7
AB-Yusuf <engryusufmayowa@gmail.com>
2019-09-25 19:06:52 +0100

    Code Structure:
    Added a new function called get_last_blockchain_value().
    This is to show that a function carries out a specified action.

--- blockChain.py
+++ blockChain.py
@@ -1,15 +1,17 @@
 blockChain = [1]
 
 
-#To add a value to the block chain List
-def add_value():
-    blockChain.append([blockChain[-1], 5.3])
-    print(blockChain)
-    
-    
-  
-add_value()
-add_value()
-add_value()
-print([blockChain[0]])
+def get_last_blockchain_value():
+    return blockChain[-1]
 
+
+# To add a value to the block chain List
+def add_value(transaction_amount):
+    blockChain.append([get_last_blockchain_value(), transaction_amount])
+
+
+add_value(2)
+add_value(3)
+add_value(4)
+
+print(blockChain)
